fa8998d163d5a82f02ef8222598611907d7e325b
araj-abood <alaraj.abdullah02@gmail.com>
2024-01-31 12:28:33 +0300

    New Build

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -7,7 +7,7 @@ function AboutMe() {
     <div className="w-[80%]  mx-auto px-3 py-2 rounded-md border-white/40 border-[1px] bg-red-900/10">
       <h2 className="text-center text-3xl text-red-50 mb-2">About me</h2>
       <div className="px-3">
-        <h1 className="mb-5 ">
+        <h1 className="mb-5 text-center">
           I am a computer science student, i study in the American university of
           madaba
         </h1>
@@ -15,7 +15,7 @@ function AboutMe() {
           My hobbies
         </h2>
 
-        <ol className="flex justify-center gap-10">
+        <ol className="grid grid-cols-3 mt-5 ">
           <HobbiesItem text={"League of legends"} />
           <HobbiesItem text={"Programming"} />
           <HobbiesItem text={"Anime"} />
--- src/components/about-me/HobbiesItem.jsx
+++ src/components/about-me/HobbiesItem.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function HobbiesItem({ text }) {
-  return <li className="underline">{text}</li>;
+  return (
+    <li className="underline flex justify-center text-xl hover:text-red-400 transition-all duration-300 ">
+      {text}
+    </li>
+  );
 }
 
 export default HobbiesItem;
