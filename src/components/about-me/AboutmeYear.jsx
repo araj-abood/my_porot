3a79e24d3cc1b581a8bbf1a856708d7b3d2e4d02
araj-abood <alaraj.abdullah02@gmail.com>
2024-01-31 22:34:29 +0300

    fixed colors

--- src/components/about-me/AboutmeYear.jsx
+++ src/components/about-me/AboutmeYear.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function AboutmeYear({ details }) {
   return (
     <div className="min-w-[250px] min-h-[150px] text-center">
-      <h2 className="text-3xl text-red-400">{details.year}</h2>
+      <h2 className="text-3xl text-main-300">{details.year}</h2>
       <ul className="text-sm text-gray-200  flex flex-col gap-1 mt-2">
         {details.done.map((d) => (
           <li>{d}</li>
--- src/components/about-me/DottedLines.jsx
+++ src/components/about-me/DottedLines.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function DottedLines() {
-  return <div className="bg-red-600 w-[50px] h-[3px] opacity-60"></div>;
+  return <div className="bg-main-500/90 w-[50px] h-[3px] opacity-60"></div>;
 }
 
 export default DottedLines;
